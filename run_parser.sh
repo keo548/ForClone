6cbf91ca4f5b7b324dc14e86540e85f74ee76cf9
keo548 <1614563795@qq.com>
2024-10-15 19:38:12 +0800

    Create run_parser.sh

---
+++ run_parser.sh
@@ -0,0 +1,34 @@
+
+#!/bin/bash
+# change to your /full/path/to/antlr-4.13.1-complete.jar
+export LD_LIBRARY_PATH='.:/usr/local/lib:$LD_LIBRARY_PATH'
+export CLASSPATH='.:/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH'
+antlr4='java org.antlr.v4.Tool'
+grun='java org.antlr.v4.gui.TestRig'
+
+if [[ $# != 1 ]]; then
+    echo "Usage: ./run.sh java|cpp"
+    echo "Example: cat test/test_cases/simple.c1 | ./run.sh java"
+fi
+
+rm -rf build
+mkdir -p build
+
+if [[ $1 == "java" ]]; then
+    cd grammar
+    $antlr4 C1Parser.g4 -o ../build
+    cd ../build
+    javac *.java
+    $grun C1Parser tokens -tokens
+elif [[ $1 == "cpp" ]]; then
+    cd grammar
+    $antlr4 C1Parser.g4 -Dlanguage=Cpp -o ../build
+    cd ../build
+    # uncomment below if you run this file on your machine
+    #cp ../Libs_for_c1r_ref/libantlr4-runtime.so.4.13.1 /usr/local/lib/
+    #ln -snf /usr/local/lib/libantlr4-runtime.so.4.13.1 /usr/local/lib/libantlr4-runtime.so
+    #g++ -std=c++17 ../test/parser.cpp C1parser.cpp -I../include/antlr4-runtime-4.13.1 -I. -lantlr4-runtime -o c1parser
+    #./c1lexer
+    g++ -std=c++17 ../test/parser.cpp C1parser.cpp -I../include/antlr4-runtime-4.13.1 -I. -lantlr4-runtime -o c1parser
+    ./c1lexer
+fi
